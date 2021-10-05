7c24464fed55254b3bd14ccc6fb6a6d3e4bb51b6
Stu Charlton <scharlton@vmware.com>
2021-10-05 05:39:27 -0600

    updated clusters

--- azure/aks-cluster2.tf
+++ azure/aks-cluster2.tf
@@ -16,7 +16,7 @@ resource "azurerm_kubernetes_cluster" "cluster2" {
   location                = var.location
   resource_group_name     = azurerm_resource_group.platform.name
   dns_prefix              = "${var.environment_name}-c2"
-  private_cluster_enabled = true
+  private_cluster_enabled = false
   
   default_node_pool {
     name       = "default"
@@ -49,10 +49,9 @@ resource "azurerm_kubernetes_cluster_node_pool" "cluster2" {
   kubernetes_cluster_id = azurerm_kubernetes_cluster.cluster2.id
   vm_size               = "Standard_D4s_v3"
   availability_zones    = [1, 2, 3]
-  enable_auto_scaling   = true
+  enable_auto_scaling   = false
   os_disk_size_gb       = 1024
-  min_count             = 3
-  max_count             = 6
+  node_count             = 8
   vnet_subnet_id = azurerm_subnet.cluster2.id 
   tags = merge(
     var.tags,
